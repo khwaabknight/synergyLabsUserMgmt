320bd18d3945d565e1d128c3da17cddc8ce2ed78
Aman Gupta <khwaabknight@gmail.com>
2024-10-06 14:14:03 +0530

    build fixes

--- src/components/DeleteConfirmation.tsx
+++ src/components/DeleteConfirmation.tsx
@@ -1,7 +1,13 @@
-// src/components/DeleteConfirmation.js
-import React from 'react';
 
-function DeleteConfirmation({ user, onConfirm, onCancel }) {
+type DeleteConfirmationProps = {
+  user: User | null;
+  onConfirm: () => void;
+  onCancel: () => void;
+};
+function DeleteConfirmation({ user, onConfirm, onCancel }: DeleteConfirmationProps) {
+  if(!user) {
+    return null;
+  }
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
       <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
